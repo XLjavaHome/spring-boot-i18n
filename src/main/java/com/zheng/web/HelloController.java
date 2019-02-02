3d21e03310501837458bb0cbcdcacbde21e58d37
xuli <78645574@qq.com>
2019-02-02 17:41:00 +0800

    加上lombok

--- src/main/java/com/zheng/web/HelloController.java
+++ src/main/java/com/zheng/web/HelloController.java
@@ -1,6 +1,7 @@
 package com.zheng.web;
 
 import com.zheng.utils.LocaleMessageSourceUtil;
+import lombok.extern.log4j.Log4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by zhenglian on 2016/10/9.
  */
 @Controller
+@Log4j
 public class HelloController {
     @Autowired
     private LocaleMessageSourceUtil messageSourceUtil;
@@ -16,7 +18,8 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
         String welcome = messageSourceUtil.getMessage("welcome");
-        System.out.println(welcome);
+        log.info(welcome);
+        log.info("测试");
         return "hello";
     }
 }
